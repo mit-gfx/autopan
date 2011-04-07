8ed9bda0a8b90547b2091c7a7b1441431156ba3b
jrk <jrk@43d2f838-a88d-4c97-9f72-fadf64bd5620>
2011-04-07 21:37:18 +0000

    Added basic Xcode project which links OpenCV
    git-svn-id: https://svn.csail.mit.edu/drakeley@2 43d2f838-a88d-4c97-9f72-fadf64bd5620

--- VideoAverage/VideoAverage/main.cpp
+++ VideoAverage/VideoAverage/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main (int argc, const char * argv[])
 {
+    // Some arbitrary width/height for testing.
+    int width = 720, height = 480;
 
+    // Create an image.
+    // IPL_DEPTH_32F tells it to use 32-bit floats for the image, so we don't run out of precision
+    IplImage* image = cvCreateImage(cvSize(width, height), IPL_DEPTH_32F, 3);
+    
     // insert code here...
+    // Delete the above as necessary
     std::cout << "Hello, World!\n";
+    std::cout << "Image size: " << image->width << "x" << image->height << "\n";
     return 0;
 }
