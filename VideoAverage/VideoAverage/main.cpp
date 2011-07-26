8a80b9fb768eb36d2491cbf77fa09fb8f368b318
zefu lu <luzefu19941231@gmail.com>
2011-07-26 16:30:52 -0400

    Alpha and Beta Changing( Not sure if it is right)

--- VideoAverage/VideoAverage/main.cpp
+++ VideoAverage/VideoAverage/main.cpp
@@ -5,6 +5,9 @@ using std::vector;
 using namespace cv;
 
 bool record = false;
+double alpha = 1.0;
+double beta = 0.0;
+Mat dst;
 
 int main(int, char**)
 {
@@ -12,30 +15,40 @@ int main(int, char**)
     
     VideoCapture cap(0); 
     if(!cap.isOpened())  
-    return -1;
+    return -1;      //test the camera
     
-    Mat edges;
-    namedWindow("video",1);
+    namedWindow("Video");  //create a window
         
     while(record == false)
     {
-         if(waitKey('r') >= 0) record =true;
+         if(waitKey('r') == 82) record =true;    //start the record
     }
     
     while(record == true)
     {
         Mat frame;
         cap.read(frame);
-        imshow("video", frame);
+        imshow("Video", frame);
         frames.push_back(frame);
-        
-        if(waitKey('s') >=0 ) record =false;
+        if(waitKey('s') == 83) record =false;   //end the record
     }
+    
+    //blending
+    Mat prevBlend = Mat::zeros(frames[0].size(), frames[0].type());
     
     for (int f = 0; f < frames.size(); f++) {
         Mat frame = frames[f];
-        // blend together, analyze, etc., etc.
+        printf("src1.type: %d, src2.type: %d; src1.depth: %d\n", frame.type(), prevBlend.type(), frame.depth());  //check
+        addWeighted( frame, alpha, prevBlend, beta, 0.0, dst);
+        prevBlend = dst;
+           
+        alpha = 1.0 / (f + 2);
+        beta = 1.0 - alpha;
     }
-  
+
+    imwrite("/Users/yanling/Documents/autopan/blend.png", dst);    //save blended image
+    
+    imshow( "Video", dst);  //show blended image
+    
     return 0;
 }
